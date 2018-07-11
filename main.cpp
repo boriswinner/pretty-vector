6404815d3c24f4c7ba35307cb54f9679865771ba
Boris Timofeenko <timbori@mail.ru>
2018-07-11 20:46:24 +1000

    tryin' 2 make iterator ok

--- main.cpp
+++ main.cpp
@@ -3,113 +3,6 @@
 
 namespace pretty_vector {
 
-    template<typename C, typename V>
-    class MyVectorIterator : public std::iterator<std::random_access_iterator_tag, V, int> {
-
-    public:
-        C *c;
-        unsigned int i;
-
-        MyVectorIterator(C *c) : c(c), i(0) {}
-
-        MyVectorIterator(C *c, unsigned int i) : c(c), i(i) {}
-
-    public:
-        typedef typename std::iterator<std::random_access_iterator_tag, V, int>::pointer pointer;
-        typedef typename std::iterator<std::random_access_iterator_tag, V, int>::reference reference;
-        typedef typename std::iterator<std::random_access_iterator_tag, V, int>::difference_type difference_type;
-
-        template<typename C2, typename V2>
-        MyVectorIterator(const MyVectorIterator<C2, V2> &other) : c(other.c), i(other.i) {}
-
-        template<typename C2, typename V2>
-        MyVectorIterator &operator=(const MyVectorIterator<C2, V2> &other) {
-            c = other.c;
-            i = other.i;
-            return *this;
-        }
-
-        reference operator*() const {
-            return (*c)[i];
-        }
-
-        pointer operator->() const {
-            return &(*c)[i];
-        }
-
-        MyVectorIterator &operator++() {
-            ++i;
-            return *this;
-        }
-
-        MyVectorIterator &operator--() {
-            --i;
-            return *this;
-        }
-
-        MyVectorIterator operator++(int) {
-            return MyVectorIterator(c, i++);
-        }
-
-        MyVectorIterator operator--(int) {
-            return MyVectorIterator(c, i--);
-        }
-
-        MyVectorIterator operator+(const difference_type &n) const {
-            return MyVectorIterator(c, (i + n));
-        }
-
-        MyVectorIterator &operator+=(const difference_type &n) {
-            i += n;
-            return *this;
-        }
-
-        MyVectorIterator operator-(const difference_type &n) const {
-            return MyVectorIterator(c, (i - n));
-        }
-
-        MyVectorIterator &operator-=(const difference_type &n) {
-            i -= n;
-            return *this;
-        }
-
-        reference operator[](const difference_type &n) const {
-            return (*c)[i + n];
-        }
-
-        bool operator==(const MyVectorIterator &other) const {
-            return i == other.i;
-        }
-
-        bool operator!=(const MyVectorIterator &other) const {
-            return i != other.i;
-        }
-
-        bool operator<(const MyVectorIterator &other) const {
-            return i < other.i;
-        }
-
-        bool operator>(const MyVectorIterator &other) const {
-            return i > other.i;
-        }
-
-        bool operator<=(const MyVectorIterator &other) const {
-            return i <= other.i;
-        }
-
-        bool operator>=(const MyVectorIterator &other) const {
-            return i >= other.i;
-        }
-
-        difference_type operator+(const MyVectorIterator &other) const {
-            return i + other.i;
-        }
-
-        difference_type operator-(const MyVectorIterator &other) const {
-            return i - other.i;
-        }
-    };
-
 
     template<class T, class Allocator = std::allocator<T>>
     class Vector {
@@ -122,32 +15,114 @@ namespace pretty_vector {
         using pointer = typename std::allocator_traits<Allocator>::pointer;
         using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
 
-        typedef MyVectorIterator<T, data_type> iterator;
-        typedef MyVectorIterator<const T, const data_type> const_iterator;
+        template<typename V>
+        class MyVectorIterator : public std::iterator<std::random_access_iterator_tag, V, int> {
 
-        //using iterator = VectorIterator<T, nonconstant>;
-        //using const_iterator = VectorIterator<T, constant>;
-
-
-        /*class iterator
-        {
         public:
-            typedef iterator self_type;
-            typedef T value_type;
-            typedef T& reference;
-            typedef T* pointer;
-            typedef std::forward_iterator_tag iterator_category;
-            typedef int difference_type;
-            iterator(pointer ptr) : ptr_(ptr) { }
-            self_type operator++() { self_type i = *this; ptr_++; return i; }
-            self_type operator++(int junk) { ptr_++; return *this; }
-            reference operator*() { return *ptr_; }
-            pointer operator->() { return ptr_; }
-            bool operator==(const self_type& rhs) { return ptr_ == rhs.ptr_; }
-            bool operator!=(const self_type& rhs) { return ptr_ != rhs.ptr_; }
-        private:
-            pointer ptr_;
-        };*/
+            Vector *c;
+            unsigned int i;
+
+            MyVectorIterator(Vector *c) : c(c), i(0) {}
+            MyVectorIterator(Vector *c, unsigned int i) : c(c), i(i) {}
+
+        public:
+            typedef typename std::iterator<std::random_access_iterator_tag, V, int>::pointer pointer;
+            typedef typename std::iterator<std::random_access_iterator_tag, V, int>::reference reference;
+            typedef typename std::iterator<std::random_access_iterator_tag, V, int>::difference_type difference_type;
+
+            template<typename V2>
+            MyVectorIterator(const MyVectorIterator<V2> &other) : c(other.c), i(other.i) {}
+
+            template<typename V2>
+            MyVectorIterator &operator=(const MyVectorIterator<V2> &other) {
+                c = other.c;
+                i = other.i;
+                return *this;
+            }
+
+            reference operator*() const {
+                return (*c)[i];
+            }
+
+            pointer operator->() const {
+                return &(*c)[i];
+            }
+
+            MyVectorIterator &operator++() {
+                ++i;
+                return *this;
+            }
+
+            MyVectorIterator &operator--() {
+                --i;
+                return *this;
+            }
+
+            MyVectorIterator operator++(int) {
+                return MyVectorIterator(c, i++);
+            }
+
+            MyVectorIterator operator--(int) {
+                return MyVectorIterator(c, i--);
+            }
+
+            MyVectorIterator operator+(const difference_type &n) const {
+                return MyVectorIterator(c, (i + n));
+            }
+
+            MyVectorIterator &operator+=(const difference_type &n) {
+                i += n;
+                return *this;
+            }
+
+            MyVectorIterator operator-(const difference_type &n) const {
+                return MyVectorIterator(c, (i - n));
+            }
+
+            MyVectorIterator &operator-=(const difference_type &n) {
+                i -= n;
+                return *this;
+            }
+
+            reference operator[](const difference_type &n) const {
+                return (*c)[i + n];
+            }
+
+            bool operator==(const MyVectorIterator &other) const {
+                return i == other.i;
+            }
+
+            bool operator!=(const MyVectorIterator &other) const {
+                return i != other.i;
+            }
+
+            bool operator<(const MyVectorIterator &other) const {
+                return i < other.i;
+            }
+
+            bool operator>(const MyVectorIterator &other) const {
+                return i > other.i;
+            }
+
+            bool operator<=(const MyVectorIterator &other) const {
+                return i <= other.i;
+            }
+
+            bool operator>=(const MyVectorIterator &other) const {
+                return i >= other.i;
+            }
+
+            difference_type operator+(const MyVectorIterator &other) const {
+                return i + other.i;
+            }
+
+            difference_type operator-(const MyVectorIterator &other) const {
+                return i - other.i;
+            }
+        };
+
+        typedef MyVectorIterator<data_type > iterator;
+        typedef MyVectorIterator<const data_type> const_iterator;
 
     public:
         explicit Vector(const Allocator &alloc = Allocator()) {
